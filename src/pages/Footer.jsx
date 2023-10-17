2802bee3bbaafcf6a7367044278c72b352bd7ba3
Adam <adamzain047@gmail.com>
2023-10-17 20:28:29 +0100

    adding new cv

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -29,7 +29,7 @@ const Footer = () => {
           <a href="mailto:adamzain047@gmail.com">Email</a>
         </div>
         <div>
-          <a target="_blank" href="/adam-resume.pdf">
+          <a target="_blank" href="/Software-CV_1.pdf">
             Resume
           </a>
         </div>
